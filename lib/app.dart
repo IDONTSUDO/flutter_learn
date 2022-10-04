44568ca9daa0f6ac574e93e97f7957a499be5906
Your Name <you@example.com>
2022-10-04 17:58:04 +0300

    Todo stream list

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learn/core/extensions/with_context.dart';
-import 'package:sizer/sizer.dart';
+import 'package:learn/sing_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -12,43 +11,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ExamplePage(),
-    );
-  }
-}
-
-class ExamplePage extends StatefulWidget {
-  const ExamplePage({
-    super.key,
-  });
-
-  @override
-  State<ExamplePage> createState() => _ExamplePageState();
-}
-
-class _ExamplePageState extends State<ExamplePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Sizer(
-        builder: (BuildContext context, Orientation orientation, DeviceType deviceType) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  color: Colors.red,
-                  width: 256.fw,
-                  child: Text(
-                    256.fw.toString(),
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
+      home: SignPage(),
     );
   }
 }
---
+++ lib/todo_page.dart
@@ -0,0 +1,98 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:learn/core/extensions/with_context.dart';
+
+typedef ST = List<bool>;
+
+class TodoPage extends StatefulWidget {
+  const TodoPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _TodoPageState();
+  }
+}
+
+class _TodoPageState extends State<TodoPage> {
+  final StreamController<ST> controller = StreamController<ST>.broadcast();
+  Sink<ST> get inputSt => controller.sink;
+  Stream<ST> get outputSt => controller.stream;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFD9D9D9),
+      body: StreamBuilder<ST>(
+        stream: outputSt,
+        builder: (
+          BuildContext context,
+          AsyncSnapshot<ST> snapshot,
+        ) {
+          ST? list = snapshot.hasData ? snapshot.data : [];
+          return ListView(
+            padding: const EdgeInsets.all(10),
+            children: [
+              const Text(
+                "Todo list streams",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+              ),
+              for (int i = 0; i < list!.length; i++)
+                Container(
+                  padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
+                  height: 70.fh,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      list[i] = !list[i];
+                      inputSt.add(list);
+                    },
+                    style: const ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll<Color>(Color(0xFFDA4444)),
+                      foregroundColor:
+                          MaterialStatePropertyAll<Color>(Color(0xFFFFFFFF)),
+                    ),
+                    child: Stack(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xFFD9D9D9),
+                            ),
+                            child: list[i]
+                                ? const Icon(Icons.add,
+                                    color: Color(0xFFDA4444), size: 40)
+                                : const Icon(IconData(0), size: 40),
+                          ),
+                        ),
+                        Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "todo ${i + 1}",
+                              textAlign: TextAlign.left,
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+              IconButton(
+                onPressed: () {
+                  list.add(false);
+                  inputSt.add(list);
+                },
+                iconSize: 75,
+                icon: const Icon(
+                  Icons.add,
+                  color: Color(0xff991f1f),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
